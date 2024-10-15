85215e9b3e938727f13ecff3902d73db83f31bcf
yosue <josedlh@yosues-MBP.fritz.box>
2024-10-15 16:35:59 +0200

    added logic to deal with wrong inputs

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -1,16 +1,39 @@
 def prompt(message):
     print(f'==> {message}')
     
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+
+    return False
+    
 prompt('Welcome to Calculator!')
 
 prompt("What's the first number?")
+
 number1 = input()
+
+while invalid_number(number1):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number1 = input()
 
 prompt("What's the second number?")
 number2 = input()
 
+while invalid_number(number2):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number2 = input()
+
+    
 prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide')
 operation = input()
+
+while operation not in ['1','2','3','4']:
+    prompt("Hmm... that doesn't look like a valid number.")
+    operation = input()
+
 
 match operation:
     case '1':   # '1' represents addition
