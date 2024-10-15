3fb6c56a83de086a6ac3ba7413bb9cb8947ed9f5
yosue <josedlh@yosues-MBP.fritz.box>
2024-10-15 16:21:00 +0200

    improved readability of calclator

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -1,21 +1,25 @@
-print('Welcome to Calculator!')
+def prompt(message):
+    print(f'==> {message}')
+    
+prompt('Welcome to Calculator!')
 
-print("What's the first number?")
+prompt("What's the first number?")
 number1 = input()
 
-print("What's the second number?")
+prompt("What's the second number?")
 number2 = input()
 
-print('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide')
+prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide')
 operation = input()
 
-if operation == '1':   # '1' represents addition
-    output = int(number1) + int(number2)
-elif operation == '2':
-    output = int(number1) + int(number2)
-elif operation == '3':
-    output = int(number1) * int(number2)
-elif operation == '4':
-    output = int(number1) / int(number2)
+match operation:
+    case '1':   # '1' represents addition
+        output = int(number1) + int(number2)
+    case '2':
+        output = int(number1) + int(number2)
+    case '3':
+        output = int(number1) * int(number2)
+    case '4':
+        output = int(number1) / int(number2)
 
-print(f"The result is: {output}")
+prompt(f"The result is: {output}")
