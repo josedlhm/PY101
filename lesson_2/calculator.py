d622b4f9547f2bfec08524f177ec4770035a1275
yosue <josedlh@yosues-MBP.fritz.box>
2024-10-15 17:02:33 +0200

    modified formatting

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -1,3 +1,9 @@
+"""
+hace
+
+alguna huevada
+
+"""
 def prompt(message):
     print(f'==> {message}')
     
@@ -26,23 +32,22 @@ while invalid_number(number2):
     prompt("Hmm... that doesn't look like a valid number.")
     number2 = input()
 
-    
-prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide')
+prompt("""What operation would you like to perform?
+1) Add 2) Subtract 3) Multiply 4) Divide""")
 operation = input()
 
 while operation not in ['1','2','3','4']:
     prompt("Hmm... that doesn't look like a valid number.")
     operation = input()
 
-
 match operation:
-    case '1':   # '1' represents addition
+    case '1':
         output = int(number1) + int(number2)
     case '2':
-        output = int(number1) + int(number2)
+        output = int(number1) - int(number2)
     case '3':
         output = int(number1) * int(number2)
     case '4':
         output = int(number1) / int(number2)
-
+        
 prompt(f"The result is: {output}")
